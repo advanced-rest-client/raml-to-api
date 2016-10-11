6cadb94d619143697f65879cc697bd5487462851
jarrodek <jarrodek@gmail.com>
2016-10-11 23:24:14 +0100

    Addec CORS

--- server/app.js
+++ server/app.js
@@ -10,6 +10,14 @@ const ApiCache = require('./libs/cache').ApiCache;
 const app = express();
 app.set('trust proxy', true);
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, ' +
+    'x-client-id');
+  next();
+});
+
 const sendError = (res, message, code) => {
   res.set('Content-Type', 'application/json');
   res.status(code).send({
